190e38aee21c72d3bb5aeb10d22fb724fb0e948c
Ayu <Xxxxxvxxvxxx+b@gmail.com>
2025-12-11 17:53:07 +0530

    Add files via upload

---
+++ e/class11/e_thumbnail.js
@@ -0,0 +1,25 @@
+function name_p_n_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_physics_chap").classList.add("active");
+  setActiveIcon("class11_physics_chap");
+}
+function name_c_n_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_chemistry_chap").classList.add("active");
+  setActiveIcon("class11_chemistry_chap");
+}
+function name_b_n_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_biology_chap").classList.add("active");
+  setActiveIcon("class11_biology_chap");
+}
+function name_p_fs_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_physics_formulaSheet_chap").classList.add("active");
+  setActiveIcon("class11_physics_formulaSheet_chap");
+}
+function name_c_fs_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_chemistry_formulaSheet_chap").classList.add("active");
+  setActiveIcon("class11_chemistry_formulaSheet_chap");
+}
